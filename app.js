9568d63a34d41027a3e4009ead2ef04bea2357e1
Carlos D. Ascencio <aa15009@ues.edu.sv>
2022-04-12 15:45:50 -0600

    Test ESLINT pre commit: app.js & www modified

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 
 const app = express()
-
+console.log('Error')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
