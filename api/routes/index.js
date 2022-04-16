c35c5e4e8ea3d056b759316e871616ac94cd46a2
Carlos D. Ascencio <aa15009@ues.edu.sv>
2022-04-15 22:03:13 -0600

    Demostration Endpoints para Sex

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,13 @@
 const Router = require('express').Router
 const router = Router()
+const usersRouter = require('./users')
+const sexesRouter = require('./sex')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' })
 })
+router.use('/users', usersRouter)
+router.use('/sexes', sexesRouter)
 
 module.exports = router
